0d8dab772a26c8953329634d6d068d739e3f5c01
joker-jonesy <spashmcgrath94@yahoo.com>
2023-08-10 10:10:01 -0400

    kermit

---
+++ app.js
@@ -0,0 +1,10 @@
+let num =2; num=3;
+let name = "Luke Jones";
+let name2= "Selene"
+name2= "Celene"
+name=name+" "+name2;
+let num2 = 4;
+num = (num+1)*(num2/2);
+num2=num+5;
+const samey = 22;
+console.log(name)
